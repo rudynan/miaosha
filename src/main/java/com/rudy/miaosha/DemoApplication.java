3e141568eea0095101d832ce224a517a630ec4d6
nanhaoxuan <nanhx@fapiao.com>
2019-05-28 21:41:00 +0800

    添加登录功能_miaosha_nanhx

--- src/main/java/com/rudy/miaosha/DemoApplication.java
+++ src/main/java/com/rudy/miaosha/DemoApplication.java
@@ -17,13 +17,6 @@ public class DemoApplication {
     public static void main(String[] args) {
         SpringApplication.run(DemoApplication.class, args);
     }
-    @Bean
-    public JedisPool jedisPoolFactory() {
-        JedisPoolConfig jedisPoolConfig = new JedisPoolConfig();
-        jedisPoolConfig.setMaxIdle(redisConfig.getPoolMaxIdle());
-        jedisPoolConfig.setMaxTotal(redisConfig.getPoolMaxTotal());
-        jedisPoolConfig.setMaxWaitMillis(redisConfig.getPoolMaxWait()*1000);
-        JedisPool jedisPool = new JedisPool(jedisPoolConfig, redisConfig.getHost(), redisConfig.getPort(), redisConfig.getTimeout() * 1000, redisConfig.getPassword(),10);
-        return jedisPool;
-    }
+
+
 }
