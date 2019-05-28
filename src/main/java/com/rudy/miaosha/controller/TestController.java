aad706276c4b179c95064f31161514d6c642a9eb
nanhaoxuan <nanhx@fapiao.com>
2019-05-28 21:39:18 +0800

    添加登录功能_miaosha_nanhx

--- src/main/java/com/rudy/miaosha/controller/TestController.java
+++ src/main/java/com/rudy/miaosha/controller/TestController.java
@@ -1,10 +1,12 @@
 package com.rudy.miaosha.controller;
 
 import com.rudy.miaosha.domain.UserInfo;
+import com.rudy.miaosha.redis.UserKey;
 import com.rudy.miaosha.result.CodeMsg;
 import com.rudy.miaosha.result.Result;
 import com.rudy.miaosha.service.RedisService;
 import com.rudy.miaosha.service.UserService;
+import com.sun.org.apache.xpath.internal.operations.Bool;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
@@ -40,9 +42,17 @@ public class TestController {
 
     @RequestMapping("/redis/get")
     @ResponseBody
-    public Result<Long> redisGet() throws Exception {
-        Long key1 = redisService.get("key1", Long.class);
+    public Result<String> redisGet() throws Exception {
+        String  key1 = redisService.get(UserKey.getById,"key1", String.class);
         return Result.success(key1);
+
+    }
+
+    @RequestMapping("/redis/set")
+    @ResponseBody
+    public Result<Boolean> redisSet() throws Exception {
+         redisService.set(UserKey.getById,"key1", "valuessssss");
+        return Result.success(true);
 
     }
 }
---
+++ src/main/java/com/rudy/miaosha/controller/UserController.java
@@ -0,0 +1,30 @@
+package com.rudy.miaosha.controller;
+
+import com.rudy.miaosha.result.Result;
+import com.rudy.miaosha.vo.LoginVo;
+import com.sun.org.apache.xpath.internal.operations.Bool;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.ResponseBody;
+
+@Controller
+public class UserController {
+
+    private static Logger logger = LoggerFactory.getLogger(UserController.class);
+
+    @GetMapping(value = "/login")
+    public String  tologin() {
+        return "login";
+    }
+
+    @PostMapping(value = "/login/do_login")
+    @ResponseBody
+    public Result<Boolean> login(LoginVo loginVo) {
+        logger.info("login接口入参为:{}",loginVo);
+        return null;
+    }
+}
