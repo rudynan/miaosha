6db91a9197f0514737c56d1833cf54bf4153b4e8
nanhaoxuan <nanhx@fapiao.com>
2019-05-29 10:24:46 +0800

    添加webjar依赖_miaosha_nanhx

---
+++ src/main/java/com/rudy/miaosha/adapter/ApplicationConfig.java
@@ -0,0 +1,13 @@
+package com.rudy.miaosha.adapter;
+
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter;
+
+@Configuration
+public class ApplicationConfig extends WebMvcConfigurerAdapter {
+    @Override
+    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+        registry.addResourceHandler("/").addResourceLocations("login.html");
+    }
+}
