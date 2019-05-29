31ad1e3e417e2bacb1b2a40709add88928855ba9
nanhaoxuan <nanhx@fapiao.com>
2019-05-29 10:56:19 +0800

    添加映射配置文件_miaosha_nanhx

--- src/main/java/com/rudy/miaosha/adapter/ApplicationConfig.java
+++ src/main/java/com/rudy/miaosha/adapter/ApplicationConfig.java
@@ -6,8 +6,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurerAdapter
 
 @Configuration
 public class ApplicationConfig extends WebMvcConfigurerAdapter {
-    @Override
-    public void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("/").addResourceLocations("login.html");
-    }
+//    @Override
+//    public void addResourceHandlers(ResourceHandlerRegistry registry) {
+//        registry.addResourceHandler("/**").addResourceLocations("login");
+//    }
 }
